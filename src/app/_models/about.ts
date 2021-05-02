b61006b2a0056e2814acb79c4f4b1d3592c92e0a
zcreative <ininalkart2@gmail.com>
2021-05-02 09:58:27 +0300

    aboutservice yazılıyor...

---
+++ src/app/_models/about.ts
@@ -0,0 +1,6 @@
+export class Aboutus
+{
+
+  message?:string;
+
+}
--- src/app/_services/about.service.ts
+++ src/app/_services/about.service.ts
@@ -1,10 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Aboutus } from '../_models/about';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
+  }),
+};
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AboutService {
+  private baseUrl: string = 'http://localhost:5000/about/';
+  constructor(private http: HttpClient) {}
 
-constructor() { }
+  getAbout(): Observable<Aboutus> {
+    return this.http.get<Aboutus>(this.baseUrl);
+  }
 
+  putAbout(_about: Aboutus): Observable<Aboutus> {
+    return this.http.put<Aboutus>(this.baseUrl, httpOptions);
+  }
 }
