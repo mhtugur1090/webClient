327f4ecd02a315cf7cf5d0e410d854c861e26fef
zcreative <ininalkart2@gmail.com>
2021-05-15 13:49:18 +0300

    loadin page added

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -6,26 +6,32 @@ import { ProductService } from '../_services/product.service';
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.css'],
 })
 export class ProductDetailComponent implements OnInit {
-
-  product?:Product;
-  constructor(private productService:ProductService,private route:ActivatedRoute) { }
+  product?: Product;
+  loading: boolean = false;
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.getProduct();
   }
 
-  getProduct()
-  {
-this.productService.getProduct(this.route.snapshot.params["id"]).subscribe(result=>
-  {
-    this.product = result;
-  },err=>
-  {
-    console.log(err);
-  });
-  }
+  getProduct() {
+    this.loading = true;
+    this.productService.getProduct(this.route.snapshot.params['id']).subscribe(
+      (result) => {
+        this.product = result;
+        this.loading = false;
+      },
+      (err) => {
 
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
 }
